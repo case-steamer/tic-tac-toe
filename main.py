e4d0606391f79b3400f2481646ea678d10a21af7
James Haining <johndoe@example.com>
2025-07-10 15:58:29 -0500

    stuck on button click function @ line 29, need time to think

--- main.py
+++ main.py
@@ -17,12 +17,16 @@ game_spaces = [
     (3, 1),
     (3, 2)
     ]
+buttons = []
 
 
 
 
 
-
+def button_click(image, coords):
+    label = Label(root, image=image, highlightthickness=0, bd=0)
+    for x,y in [coords]:
+        button = [b for b in buttons if b[1] == x,y]
 
 
 
@@ -41,6 +45,10 @@ header_img = Image.open('images/header.jpg')
 header = ImageTk.PhotoImage(header_img)
 cell_img = Image.open('images/cell.jpg')
 cell = ImageTk.PhotoImage(cell_img)
+draw_X = Image.open('images/X.png')
+X_img = ImageTk.PhotoImage(draw_X)
+draw_O = Image.open('images/O.png')
+O_img = ImageTk.PhotoImage(draw_O)
 
 board_label = Label(root, image=header, highlightthickness=0, bd=0)
 
@@ -53,6 +61,7 @@ for tup in game_spaces:
         row = x
         column = y
     cell_button.grid(row=row, column=column, columnspan=1, rowspan=1, sticky='nsew')
+    buttons.append((cell_button, tup))
 
 
 
