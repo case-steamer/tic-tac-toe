9e8e859e6b86145e2b6fb150bed0c0cbff8e40b2
James Haining <johndoe@example.com>
2025-07-16 17:45:37 -0500

    still working on check_for_three() function. Done for the day.

--- main.py
+++ main.py
@@ -20,6 +20,12 @@ game_spaces = [
     ]
 buttons = []
 count = 1
+xos = 0
+xts = 0
+xfs = 0
+yts = 0
+yfs = 0
+yss = 0
 
 
 
@@ -50,11 +56,59 @@ O_img = ImageTk.PhotoImage(draw_O)
 canvas = Canvas(root, width=header.width(), height=(header.height() * 4))
 canvas.grid(row=0, column=0, sticky='nsew')
 
-p1 = Player(name='Player 1', avatar=draw_X)
-p2 = Player(name='Player 2', avatar=draw_O)
+p1 = Player(name='Player 1', avatar=draw_X, status='')
+p2 = Player(name='Player 2', avatar=draw_O, status='')
 
 
-
+def check_for_three(player:Player, cell_x, cell_y):
+    print((cell_x, cell_y))
+    global xos
+    global xts
+    global xfs
+    global yts
+    global yfs
+    global yss
+    x_local = [num for num in range(100, 600, 200)]
+    y_local = [num for num in range(300, 800, 200)]
+    print(x_local)
+    print(y_local)
+    for num in x_local:
+        if cell_x == num and num == 100:
+            xos += 1
+            # break
+        elif cell_x == num and num == 300:
+            xts += 1
+            # break
+        elif cell_x == num and num == 500:
+            xfs += 1
+            # break
+    for y in y_local:
+        if cell_y ==  y and y == 300:
+            yts += 1
+            # break
+        elif cell_y == y and y == 500:
+            yfs += 1
+            # break
+        elif cell_y == y and y == 700:
+            yss += 1
+            # break
+    x_cell_score = [xos, xts, xfs]
+    y_cell_score = [yts, yfs, yss]
+    print(x_cell_score)
+    print(y_cell_score)
+    if sum(x_cell_score) == sum(y_cell_score) == 3:
+        player.status = 'wins'
+        marker = True
+    else:
+        for x in x_cell_score:
+            for y in y_cell_score:
+                if x != 3 and y != 3:
+                    marker = False
+                else:
+                    player.status = 'wins!'
+                    marker = True
+    print(marker)
+    return marker
 
 
 
@@ -75,9 +129,12 @@ def button_click(button):
     display_lbl = Label(root, image=display_img, highlightthickness=0)
     display_lbl.image = display_img
     canvas.create_window(x_loc[0], y_loc[0], window=display_lbl, width=200, height=200, anchor='center')
-    buttons_to_remove = next((b for b in buttons if b[0] == button), None)
-    if buttons_to_remove is not None:
-        buttons.remove(buttons_to_remove)
+    button_to_remove = next((b for b in buttons if b[0] == button), None)
+    if button_to_remove is not None:
+        current_player.cells.append(button_to_remove[1])
+        buttons.remove(button_to_remove)
+    if check_for_three(current_player, x_loc, y_loc):
+        print(current_player.name + ' ' + current_player.status)
     count += 1
 
 def generate_button(x, y):
--- player.py
+++ player.py
@@ -3,7 +3,11 @@ from PIL import Image
 class Player:
     name: str
     avatar: Image
+    cells: list
+    status: str
 
-    def __init__(self, name: str, avatar:Image):
+    def __init__(self, name: str, avatar:Image, status:str):
         self.name = name
         self.avatar = avatar
+        self.cells = []
+        self.status = status
